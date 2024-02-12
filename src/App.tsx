83945255d691d1368e06adcb0718862352deafc2
Justin Phan <justin@MacBook-Pro-cua-Justin.local>
2024-02-12 08:49:31 +0700

    fix build

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import Grid from '@mui/material/Grid';
 import { TreeView } from './components/TreeView';
-import { mockData as node } from './mock';
 import { Canvas } from './components/Canvas';
 import CanvasContextProvider from './context/CanvasContext';
 
@@ -9,13 +8,13 @@ function App() {
     <CanvasContextProvider>
       <Grid container spacing={2}>
         <Grid item xs={4} lg={3}>
-          <TreeView node={node}  />
+          <TreeView />
         </Grid>
         <Grid item xs={8} lg={9}>
-          <Canvas node={node} />
+          <Canvas />
         </Grid>
       </Grid>
-    </CanvasContextProvider>    
+    </CanvasContextProvider>
   )
 }
 
